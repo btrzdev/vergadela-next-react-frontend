02e65a7baa33a626fdd5af5774340f3ea204149a
btrzdev <btrz.dev@gmail.com>
2024-08-14 16:17:56 +0100

    types of projects page draft

--- src/pages/project-types/index.tsx
+++ src/pages/project-types/index.tsx
@@ -8,27 +8,52 @@ const ProjectTypes = () => {
       className={`flex min-h-screen flex-col items-center justify-between bg-white`}
     >
       <NavBar />
-      <div className="flex h-full w-full flex-col items-center justify-center bg-white pb-[196px]">
-        <div className="flex flex-col items-center justify-center">
+      <div className="flex h-full w-full flex-col items-center justify-center bg-white">
+        <div className="flex flex-col items-center justify-center gap-[106px]">
           <h1 className="font-glittenCaps text-[70px] font-normal text-black">
             Tipos de Projetos
           </h1>
-          <p>
+          <p className="inline-block max-w-[630px] text-center text-[28px] leading-[48px]">
             "Seja em um apartamento aconchegante, uma moradia espaçosa ou um
             ambiente corporativo dinâmico, tudo é feito com{' '}
-            <span>amor e dedicação</span>."
+            <span className="font-glittenCaps">amor e dedicação</span>."
           </p>
           <hr className="h-[2px] w-[87px] bg-primary-yellow" />
         </div>
-        <div className="flex w-full flex-col">
-          <div className="flex w-full items-center justify-center">
-            <p>Apartamentos</p>
+        <div className="align-self-end flex h-[439px] w-full flex-col lg:flex-row">
+          <div
+            className="h-full] flex w-[33%] w-full items-center justify-center"
+            style={{
+              backgroundImage: `url('/images/component_24.png')`,
+              backgroundRepeat: 'no-repeat',
+              backgroundSize: 'cover',
+            }}
+          >
+            <p className="font-glittenCaps text-[50px] text-white">
+              Apartamentos
+            </p>
           </div>
-          <div className="flex w-full items-center justify-center">
-            <p>Moradias</p>
+          <div
+            className="flex h-full w-[33%] w-full items-center justify-center"
+            style={{
+              backgroundImage: `url('/images/component_25.png')`,
+              backgroundRepeat: 'no-repeat',
+              backgroundSize: 'cover',
+            }}
+          >
+            <p className="font-glittenCaps text-[50px] text-white">Moradias</p>
           </div>
-          <div className="flex w-full items-center justify-center">
-            <p>Corporativo</p>
+          <div
+            className="flex h-full w-[33%] w-full items-center justify-center"
+            style={{
+              backgroundImage: `url('/images/component_26.png')`,
+              backgroundRepeat: 'no-repeat',
+              backgroundSize: 'cover',
+            }}
+          >
+            <p className="font-glittenCaps text-[50px] text-white">
+              Coorporativo
+            </p>
           </div>
         </div>
       </div>
